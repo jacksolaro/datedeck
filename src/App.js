5b2f64cf8e82effbc9364bc3b9e63bb1d1201db3
jacksolaro <jacksolaro@gmail.com>
2021-02-22 09:48:37 -0800

    added card boiler plate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import DateCard from "./Components/DateCard/DateCard";
 
 function App() {
   return (
     <div>
-      <h1>Test</h1>
+      <DateCard></DateCard>
     </div>
   );
 }
---
+++ src/Components/DateCard/DateCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./DateCard.css";
+
+function DateCard() {
+  return (
+    <div className="DateCard__Card">
+      <h1>TYPE OF QUESTION</h1>
+      <hr></hr>
+      <h2>This is a sample question.</h2>
+    </div>
+  );
+}
+
+export default DateCard;
