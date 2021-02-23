0d71b009e58c78baea364a25d7938ea5aa7ecc52
jacksolaro <jacksolaro@gmail.com>
2021-02-23 09:59:34 -0800

    added questions to repeat

--- src/Components/DateCard/DateCard.js
+++ src/Components/DateCard/DateCard.js
@@ -4,14 +4,17 @@ import database from "../../firebase.js";
 
 function DateCard() {
   const [cards, setCards] = useState([]);
+  const [cardIndex, setCardIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   //   Piece of code which runs based on a condition
   useEffect(() => {
     const unsubscribe = database
-      .collection("cards")
-      .onSnapshot((snapshot) =>
-        setCards(snapshot.docs.map((doc) => doc.data()))
-      );
+      .collection("cardDeck")
+      .onSnapshot((snapshot) => {
+        setCards(snapshot.docs.map((doc) => doc.data()));
+        setLoading(false);
+      });
     console.log("Data", cards);
 
     return () => {
@@ -21,15 +24,26 @@ function DateCard() {
   }, []);
 
   const handleClick = () => {
-    console.log("Click!");
+    if (cardIndex >= cards.length - 1) {
+      setCardIndex(0);
+    } else {
+      setCardIndex(cardIndex + 1);
+    }
+    console.log(cardIndex);
   };
 
   return (
     <div className="DateCard__Card">
-      <h1>{cards[0].questionCategory}</h1>
-      <hr></hr>
-      <h2>{cards[0].questionText}</h2>
-      <button onClick={handleClick}>Next</button>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <>
+          <h1>{cards[cardIndex].questionCategory}</h1>
+          <hr></hr>
+          <h2>{cards[cardIndex].questionText}</h2>
+          <button onClick={handleClick}>Next</button>
+        </>
+      )}
     </div>
   );
 }
