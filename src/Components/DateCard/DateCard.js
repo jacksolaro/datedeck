75507fdbf1b1d85e776d21e573306e7c12a44c1a
jacksolaro <jacksolaro@gmail.com>
2021-02-23 10:20:11 -0800

    moved card logic outside of component and passed details in as props

--- src/Components/DateCard/DateCard.js
+++ src/Components/DateCard/DateCard.js
@@ -2,48 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./DateCard.css";
 import database from "../../firebase.js";
 
-function DateCard() {
-  const [cards, setCards] = useState([]);
-  const [cardIndex, setCardIndex] = useState(0);
-  const [loading, setLoading] = useState(true);
-
-  //   Piece of code which runs based on a condition
-  useEffect(() => {
-    const unsubscribe = database
-      .collection("cardDeck")
-      .onSnapshot((snapshot) => {
-        setCards(snapshot.docs.map((doc) => doc.data()));
-        setLoading(false);
-      });
-    console.log("Data", cards);
-
-    return () => {
-      //   This is the clean up
-      unsubscribe();
-    };
-  }, []);
-
-  const handleClick = () => {
-    if (cardIndex >= cards.length - 1) {
-      setCardIndex(0);
-    } else {
-      setCardIndex(cardIndex + 1);
-    }
-    console.log(cardIndex);
-  };
-
+function DateCard(props) {
   return (
     <div className="DateCard__Card">
-      {loading ? (
-        <h1>Loading</h1>
-      ) : (
-        <>
-          <h1>{cards[cardIndex].questionCategory}</h1>
-          <hr></hr>
-          <h2>{cards[cardIndex].questionText}</h2>
-          <button onClick={handleClick}>Next</button>
-        </>
-      )}
+      <h1>{props.cardDetails.questionCategory}</h1>
+      <hr></hr>
+      <h2>{props.cardDetails.questionText}</h2>
     </div>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DateCard from "../../Components/DateCard/DateCard";
+import database from "../../firebase.js";
 
 function Home() {
+  const [cards, setCards] = useState([]);
+  const [cardIndex, setCardIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  //   Piece of code which runs based on a condition
+  useEffect(() => {
+    const unsubscribe = database
+      .collection("cardDeck")
+      .onSnapshot((snapshot) => {
+        setCards(snapshot.docs.map((doc) => doc.data()));
+        setLoading(false);
+      });
+    console.log("Data", cards);
+
+    return () => {
+      //   This is the clean up
+      unsubscribe();
+    };
+  }, []);
+
+  const handleClick = () => {
+    if (cardIndex >= cards.length - 1) {
+      setCardIndex(0);
+    } else {
+      setCardIndex(cardIndex + 1);
+    }
+    console.log(cardIndex);
+  };
+
   return (
     <div>
-      <DateCard></DateCard>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <DateCard cardDetails={cards[cardIndex]}></DateCard>
+      )}
+      <button onClick={handleClick}>Next</button>
     </div>
   );
 }
